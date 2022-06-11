3fc078060c39843f73f0742cf986b16074731a65
Hikuroshi <92168188+Hikuroshi@users.noreply.github.com>
2022-06-11 17:21:15 +0700

    update print data

---
+++ application/views/siswa/biodata-siswa4.php
@@ -0,0 +1,438 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard siswa</title>
+
+    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container col-md-6">
+        <div class="text-center my-5">
+            <h4>
+                LAPORAN PENCAPAIAN KOMPETENSI PESERTA DIDIK <br>									
+                SMK NEGERI 1 KRAGILAN KABUPATEN SERANG <br> 									
+                TAHUN PELAJARAN 2021/2022									
+            </h4>
+        </div>
+        <div class="row">
+            <div class="col-md-8">
+                <table class="table table-borderless">
+                    <tr>
+                        <td>Nama peserta didik</td>
+                        <td>: Agus</td>
+                    </tr>
+                    <tr>
+                        <td>NISN / NIS</td>
+                        <td>: 12345678 / 0987654321</td>
+                    </tr>
+                    <tr>
+                        <td>Kelas/Semester</td>
+                        <td>: X Akuntansi dan Keuangan Lembaga 1</td>
+                    </tr>
+                    <tr>
+                        <td>Semester</td>
+                        <td>: 2 (dua)</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+
+        <div class="">
+            <h6>A. Nilai Akademik</h6>
+            <table class="table table-bordered border-dark mb-3">
+                <thead class="table-light border-dark text-center align-middle">
+                    <tr>
+                        <th rowspan="2">No</th>
+                        <th rowspan="2">Mata Pelajaran</th>
+                        <th colspan="4">Nilai Hasil Belajar</th>
+                    </tr>
+                    <tr>
+                        <th>Pengetahuan</th>
+                        <th>Keterampilan</th>
+                        <th>Nilai Akhir</th>
+                        <th>Predikat</th>
+                    </tr>
+                </thead>
+                <tbody class="text-center">
+                    <tr>
+                        <td colspan="6" class="text-start align-baseline">
+                            <h6>A. Muatan Nasional</h6>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td class="text-start">Pendidikan Agama dan Budi Pekerti</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>A</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td class="text-start">Pendidikan Kewarganegaraan</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>A</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td class="text-start">Bahasa Indonesia</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>A</td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td class="text-start">Matematika</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>A</td>
+                    </tr>
+                    <tr>
+                        <td>5</td>
+                        <td class="text-start">Sejarah Indonesia</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>A</td>
+                    </tr>
+                    <tr>
+                        <td>6</td>
+                        <td class="text-start">Bahasa Inggris dan Bahasa Asing Lainnya</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>A</td>
+                    </tr>
+                    <tr>
+                        <td colspan="6" class="text-start align-baseline">
+                            <h6>B. Muatan Kewilayahan</h6>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td class="text-start">Seni Budaya</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>A</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td class="text-start">Pendidikan Jasmani, Olahraga dan Kesehatan</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>A</td>
+                    </tr>
+                    <tr>
+                        <td colspan="6" class="text-start align-baseline">
+                            <h6>C. Muatan Perminatan Kejuruan</h6>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="6" class="text-start align-baseline">
+                            <span>C1. Dasar Bidang Keahlian</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td class="text-start">Simulasi dan Komunikasi Digital</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>A</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td class="text-start">Ekonomi Bisnis</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>A</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td class="text-start">Administrasi Umum</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>A</td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td class="text-start">IPA</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>A</td>
+                    </tr>
+                    <tr>
+                        <td colspan="6" class="text-start align-baseline">
+                            <span>C2. Dasar Program Keahlian</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td class="text-start">Etika Profesi</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>A</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td class="text-start">Aplikasi Pengolah Angka / Spreadsheet</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>A</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td class="text-start">Akuntansi Dasar</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>A</td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td class="text-start">Perbankan Dasar</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>A</td>
+                    </tr>
+                    <tr>
+                        <td colspan="6" class="text-start align-baseline">
+                            <span>C3. Kompetensi Keahlian</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td class="text-start">Praktikum Akuntansi Perusahaan Jasa, Dagang dan Manufaktur</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>A</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td class="text-start">raktikum Akuntansi Lembaga/Instansi Pemerintah</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>A</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td class="text-start">Akuntansi Keuangan </td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>A</td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td class="text-start">Komputer Akuntansi</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>A</td>
+                    </tr>
+                    <tr>
+                        <td>5</td>
+                        <td class="text-start">Administrasi Pajak</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>A</td>
+                    </tr>
+                    <tr>
+                        <td>6</td>
+                        <td class="text-start">Produk Kreatif dan Kewirausahaan</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>90</td>
+                        <td>A</td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <h6>B. Catatan Akademik</h6>
+            <div class="border border-dark p-2 mb-3" style="min-height: 100px;">
+                <span>Ananda perlu meningkatkan kompetensi Bahasa Inggris...</span>
+            </div>
+
+            <h6>C. Praktik Kerja Lapangan</h6>
+            <table class="table border-dark table-bordered mb-3">
+                <thead class="table-light border-dark text-center align-middle">
+                    <tr>
+                        <th>No</th>
+                        <th>Mitra DU/DI</th>
+                        <th>Lokasi</th>
+                        <th>Lamanya (Bulan)</th>
+                        <th>Keterangan</th>
+                    </tr>
+                </thead>
+                <tbody class="text-center">
+                    <tr>
+                        <th>1</th>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                    </tr>
+                    <tr>
+                        <th>2</th>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <h6>D. Ekstrakurikurel</h6>
+            <table class="table border-dark table-bordered mb-3">
+                <thead class="table-light border-dark text-center align-middle">
+                    <tr>
+                        <th>No</th>
+                        <th>Kegiatan Ekstrakurikurel</th>
+                        <th>Keterangan</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th class="text-center">1</th>
+                        <td>Kegiatan Pramuka</td>
+                        <td>Melaksanakan kegiatan kepramukaan dengan Baik</td>
+                    </tr>
+                    <tr>
+                        <th class="text-center">2</th>
+                        <td>Kegiatan Futsal</td>
+                        <td>Melaksanakan kegiatan Futsal dengan Cukup Baik</td>
+                    </tr>
+                    <tr>
+                        <th class="text-center">3</th>
+                        <td>0</td>
+                        <td>0</td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <div class="col-md-6">
+                <h6>E. Ketidakhadiran</h6>
+                <table class="table border-dark table-bordered">
+                    <tr>
+                        <td class="text-center">1</td>
+                        <td>Sakit</td>
+                        <td>2 hari</td>
+                    </tr>
+                    <tr>
+                        <td class="text-center">2</td>
+                        <td>Izin</td>
+                        <td>4 hari</td>
+                    </tr>
+                    <tr>
+                        <td class="text-center">3</td>
+                        <td>Tanpa Keterangan</td>
+                        <td>4 hari</td>
+                    </tr>
+                </table>
+            </div>
+
+            <h6>F. Kenaikan Kelas</h6>
+            <div class="p-2 border border-dark mb-3">
+                <span>Ananda.........ke kelas............</span>
+            </div>
+
+            <div class="d-flex flex-row-reverse py-5">
+                <div class="row">
+                    <div class="col">
+                        <table class="table table-borderless">
+                            <tr>
+                                <td>Diberikan di</td>
+                                <td>: Serang</td>
+                            </tr>
+                            <tr>
+                                <td>Tanggal</td>
+                                <td>: 17 Juni 2022</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row justify-content-around">
+                <div class="col-3 my-5">
+                    Mengetahui,<br>
+                    Orang Tua/Wali,
+                    <div class="border-bottom border-dark py-5"></div>
+                </div>
+                <div class="col-3 my-5">
+                    Wali Kelas,
+                    <div class="py-5"></div>
+                    <div class="border-bottom border-dark fw-bold">Rio Sasko Edi, S.Pd</div>
+                    <span class="fw-bold">NIP.12345678</span>
+                </div>
+                <div class="col-12 my-5 text-center">
+                    Mengetahui,<br>
+                    Kepala Sekolah
+                    <div class="py-5"></div>
+                    <span class="fw-bold">Drs.Falati, M.Si</span><br>
+                    Pembina TK.I <br>
+                    NIP.19640311 198903 1 011
+                </div>
+            </div>
+
+            <h6>G. Deskripsi Pengembangan Karakter</h6>
+            <table class="table table-bordered border-dark mb-3">
+                <thead class="table-light border-dark text-center align-middle">
+                    <tr>
+                        <th>Karakter Yang Dibangun</th>
+                        <th>Deskripsi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Integritas</td>
+                        <td>Ananda menunjukkan kejujuran dengan tidak pernah melakukan plagiat dalam PR dan tidak mencontek saat ujian selama kegiatan belajar mengajar.</td>
+                    </tr>
+                    <tr>
+                        <td>Religius</td>
+                        <td>Ananda menunjukkan ketakwaan pada agama yang dianut dan toleran pada penganut agama yang berbeda</td>
+                    </tr>
+                    <tr>
+                        <td>Nasionalis</td>
+                        <td>Ananda menunjukkan sikap nasionalis dengan tidak pernah melakukan pelanggaran sedang dan berat sesuai aturan di sekolah.</td>
+                    </tr>
+                    <tr>
+                        <td>Mandiri</td>
+                        <td>Ananda menunjukkan sikap mandiri dengan selalu mengumpulkan tugas dan PR tepat waktu sesuai dengan arahan guru-guru mata pelajaran.</td>
+                    </tr>
+                    <tr>
+                        <td>Gotong-royong</td>
+                        <td>Ananda menunjukkan sikap gotong royong sebagai relawan dalam kegiatan bakti sosial di panti werdha Cahaya Senja.</td>
+                    </tr>
+                </tbody>
+            </table>
+            
+            <h6>G. Deskripsi Pengembangan Karakter</h6>
+            <div class="p-2 border border-dark mb-3" style="min-height: 100px;">
+                <span>Ananda menunjukkan perkembangan karakter yang baik pada pembelajaran semester ini. Selain itu, ananda menunjukkan prestasi yang cukup baik di bidang karya ilmiah karena Ananda berhasil mendapatkan penghargaan tingkat provinsi dan membanggakan sekolah dengan menjuarai karya ilmiah. Sikap rendah hati, jujur dan giat ditunjukkan Ananda selama kejuaraan berlangsung.</span>
+            </div>
+        </div>
+    </div>
+
+    <script src="../assets/js/bootstrap.bundle.min.js"></script>
+    <script>window.print();</script>
+</body>
+</html>
