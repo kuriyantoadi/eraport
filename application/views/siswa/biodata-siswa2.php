c22124fb35ef09ffca43ac50ef362c1b63f2688a
Hikuroshi <92168188+Hikuroshi@users.noreply.github.com>
2022-06-11 17:21:00 +0700

    update print data

---
+++ application/views/siswa/biodata-siswa2.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Raport Siswa</title>
+
+    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container" style="font-family: 'Times New Roman', Times, serif;">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <h4 class="fw-bold text-center" style="margin-top: 200px; margin-bottom: 100px;">
+                    RAPOR SISWA <br>
+                    SEKOLAH MENENGAH KEJURUAN <br>
+                    (SMK)
+                </h4>
+        
+                <table class="table table-borderless">
+                    <tr>
+                        <td>Nama Sekolah</td>
+                        <td>: SMK Negeri 1 Kragilan</td>
+                    </tr>
+                    <tr>
+                        <td>NPSN</td>
+                        <td>: 20607901</td>
+                    </tr>
+                    <tr>
+                        <td>NIS/NSS</td>
+                        <td>: 400350/401280411035</td>
+                    </tr>
+                    <tr>
+                        <td>Alamat Sekolah</td>
+                        <td>: Jl. Raya Serang Jakarta Km 13 Perum Graha Cisait Kragilan Kode Pos 42184 Telp. (0254) 8487966</td>
+                    </tr>
+                    <tr>
+                        <td>Kelurahan</td>
+                        <td>: Cisait</td>
+                    </tr>
+                    <tr>
+                        <td>Kecamatan</td>
+                        <td>: Kragilan</td>
+                    </tr>
+                    <tr>
+                        <td>Kota/Kabupaten</td>
+                        <td>: Kabupaten Serang</td>
+                    </tr>
+                    <tr>
+                        <td>Provinsi</td>
+                        <td>: Banten</td>
+                    </tr>
+                    <tr>
+                        <td>Website</td>
+                        <td>: smkn1kragilan@ymail.com</td>
+                    </tr>
+                    <tr>
+                        <td>E-mail</td>
+                        <td>: smkn1kragilan.indosatschool.com</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <script src="../assets/js/bootstrap.bundle.min.js"></script>
+    <script>window.print();</script>
+</body>
+</html>
