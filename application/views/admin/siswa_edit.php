84bf4b5d2dd3eadbf820599b1dbb7a4cfe5d6ef2
kuriyantoadi <melatikuning212@gmail.com>
2022-06-14 08:24:32 +0700

    update 14 juni 2022, update 4

--- application/views/admin/siswa_edit.php
+++ application/views/admin/siswa_edit.php
@@ -30,7 +30,7 @@
                   <div class="form-group row">
                     <label for="inputPassword3" class="col-sm-2 col-form-label">NISN</label>
                     <div class="col-sm-10">
-                      <input type="text" class="form-control" id="inputPassword3" value="<?= $row->nisn_siswa ?>" name="nisn" placeholder="NISN">
+                      <input type="text" class="form-control" id="inputPassword3" value="<?= $row->nisn_siswa ?>" name="nisn_siswa" placeholder="NISN">
                     </div>
                   </div>
                   <div class="form-group row">
